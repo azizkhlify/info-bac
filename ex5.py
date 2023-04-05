9c670976e611befc0f1b83dd279c99bad9e77ead
azizkhlify <110391579+azizkhlify@users.noreply.github.com>
2023-04-05 14:46:38 +0100

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,5 +1,17 @@
-def khra():
-   s=str(randint(1,9))
-   for i in range(7):
-       s+=str(randint(0,9))
-   return s
+from random import randint
+def generer():
+   return randint(10000000,99999999)
+def afichier(n):
+   k=1;ch='________';s=str(n)
+   while k<=5:
+       c=str(input('chiffre:'))
+       while len(c)!=1:
+          c=str(input('chiffre:'))
+       if s.find(c)!=-1: 
+            ch=''
+            for i in range(8):
+                if s[i]==c: ch+=c
+                else: ch+='_'
+       print(ch)
+       
+                
