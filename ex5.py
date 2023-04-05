76add6f6614cf261efafbb06fcc6352c37dd6605
azizkhlify <110391579+azizkhlify@users.noreply.github.com>
2023-04-05 14:10:31 +0100

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,26 +1,5 @@
-def mawjoud (a,t):
-    b=False;k=0
-    while k<len(t) and not(b):
-        if t[k]==a: b=True
-        else : k+=1
-    return b
-def effacer (x,y):
-    return y[:y.find(x)]+y[y.find(x)+1:]
-def anag (a,b):
-    t=True
-    while a!='' and t :
-        if mawjoud (a[0],b):
-            x=a[0]
-            a=effacer(x,a)
-            b=effacer(x,b)
-            
-        else :
-            t=not(t)
-    return t and len(a)==len(b)
-#pp
-ch1=str(input('ch1 :'))
-ch2=str(input('ch2 :'))
-if (anag(ch1,ch2)):
-    print(ch1,'et',ch2,'sont anagrammes')
-else :
-    print(ch1,'et',ch2,'ne sont pas anagrammes')
+def khra():
+   s=str(randint(1,9))
+   for i in range(7):
+       s+=str(randint(0,9))
+   return s
